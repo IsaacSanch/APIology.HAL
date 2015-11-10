edad327335e54d2f6573b6f9834ea6ab5a083482
Isaac A. Sanch <isaac.sanch@progenity.com>
2015-11-10 11:22:56 -0500

    Update some assembly and build files.

--- src/Apiology.Hal/Properties/AssemblyInfo.cs
+++ src/Apiology.Hal/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Halcyon")]
+[assembly: AssemblyTitle("Apiology.Hal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Halcyon")]
+[assembly: AssemblyProduct("Apiology.Hal")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Halcyon.Tests")]
+[assembly: InternalsVisibleTo("Apiology.Hal.Tests")]
